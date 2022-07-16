a74071a9b34c9dcf7a98bb0e26fe22ce23834268
memelopez <meme30lopez@gmail.com>
2022-07-16 17:51:12 -0600

    Add basic components

---
+++ src/components/BookList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Book from './Book';
+import InputBook from './InputBook';
+
+function BookList() {
+  return (
+    <div>
+      BookList
+      <Book />
+      <InputBook />
+    </div>
+  );
+}
+
+export default BookList;
