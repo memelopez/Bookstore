83147e9ad11d4e10453cd7dede600a8a46049362
memelopez <meme30lopez@gmail.com>
2022-07-16 18:03:24 -0600

    Add import for components and basic router structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router, Routes, Route, Link,
+} from 'react-router-dom';
+import BookList from './components/BookList';
+import Categories from './components/Categories';
+import ErrorPage from './components/ErrorPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <nav>
+          <h1 className="main-title">Bookstore</h1>
+          <div className="nav-links">
+            <Link to="/">Books</Link>
+            <Link to="/categories">Categories</Link>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<BookList />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
