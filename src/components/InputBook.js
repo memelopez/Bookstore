11ecc02668a47e9778afe86f2ae08765a2f8ccf7
memelopez <meme30lopez@gmail.com>
2022-07-16 20:49:50 -0600

    Add inputs and submit button tu form

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 function InputBook() {
   return (
-    <div>InputBook</div>
+    <div>
+      <form>
+        <input type="text" name="title" placeholder="title" />
+        <input type="text" name="author" placeholder="author" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
 
